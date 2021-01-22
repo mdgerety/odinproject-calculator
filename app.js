969264d57e1d22c6bc20af36f8d138c1b852182f
Michael Gerety <37129324+mdgerety@users.noreply.github.com>
2021-01-22 15:39:25 -0800

    basic calculator functions

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
-const addButton = document.querySelector("#addButton");
-const subtractButton = document.querySelector("#subtractButton");
-const multiplyButton = document.querySelector("#multiplyButton");
-const divideButton = document.querySelector("#divideButton");
-const display = document.querySelector(".display");
-
-let currentNum;
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator_keys');
+const display = calculator.querySelector('.calculator_display');
 
 
-function add(num1, num2) {
-    const newNum = num1 + num2;
-    return newNum;
-}
+keys.addEventListener('click', event => {
+    if (!event.target.closest('button')) return
 
-function subtract(num1, num2) {
-    const newNum = num1 - num2;
-    return newNum;
-}
+    const key = event.target;
+    const keyValue = key.textContent;
+    const displayValue = display.textContent;
+    const { type } = key.dataset;
+    const {previousKeyType} = calculator.dataset;
 
-function multiply(num1, num2) {
-    const newNum = num1 * num2;
-    return newNum;
-}
-
-function divide(num1, num2) {
-    const newNum = num1 / num2;
-    return newNum;
-}
-
-function operate() {
+    // Number Key
+    if (type === 'number') {
+        if (displayValue === '0' || previousKeyType === 'operator') {
+            display.textContent = keyValue;
+        } else {
+            display.textContent += keyValue;
+        }
+    }
     
+    // Operator Key
+    if (type === 'operator') {
+        const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+        operatorKeys.forEach(el => { el.dataset.state = ''});
+        key.dataset.state = 'selected';
+
+        calculator.dataset.firstNumber = displayValue;
+        calculator.dataset.operator = key.dataset.key;
+    }
+
+    // Equal Key
+    if (type === 'equal') {
+        // Perform a calculation
+        const firstNumber = calculator.dataset.firstNumber;
+        const operator = calculator.dataset.operator;
+        const secondNumber = displayValue;
+        display.textContent = calculate(firstNumber, operator, secondNumber);
+    }
+
+    // Clear Key
+    if (type === 'clear') {
+        display.textContent = '0';
+        delete calculator.dataset.firstNumber;
+        delete calculator.dataset.secondNumber;
+    }
+
+    calculator.dataset.previousKeyType = type;
+
+})
+
+function calculate (firstNumber, operator, secondNumber) {
+    firstNumber = parseInt(firstNumber);
+    secondNumber = parseInt(secondNumber);
+
+    if (operator === 'plus') return firstNumber + secondNumber;
+    if (operator === 'minus') return firstNumber - secondNumber;
+    if (operator === 'times') return firstNumber * secondNumber;
+    if (operator === 'divide') return firstNumber / secondNumber;
 }
-
-function updateDisplay(currentNum) {
-    display.textContent = currentNum;
-}
-
-
-
-
-
